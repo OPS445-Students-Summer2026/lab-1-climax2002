42989377b95ffb1c74cfa1bd45bdf55c8c6124e4
Clinton Folarin <ctfolarin@myseneca.ca>
2026-05-15 13:14:09 -0400

    Completed lab1

---
+++ lab1b.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+'''Description: Lab1b string objects & printing'''
+
+print('How old are you Isaac?')
---
+++ lab1c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Description: Integer objects'''
+
+name = 'Isaac'
+age = 72
+
+print(name + ' is ' + str(age) + ' years old!')
